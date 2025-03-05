6e2f08da4f3c49cb673c11adaefd048fa4b3c464
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-03-05 11:47:40 +0100

    1741171659080240451

---
+++ src/bash/field.sh
@@ -0,0 +1,6 @@
+#
+# Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+# https://kekse.biz/ https://github.com/kekse1/field/
+# v1.0.0
+#
+
---
+++ src/js/field.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+ * https://kekse.biz/ https://github.com/kekse1/field/>
+ * v1.0.0
+ */
+
+class Field
+{
+	constructor(... _args)
+	{
+		//
+		//TODO/
+		//
+	}
+}
+
+export default Field;
+
+//
+
