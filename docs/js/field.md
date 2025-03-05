261f0d5eb5209edd7cb78c26f5479a2289994d0e
Sebastian Kucharczyk <kuchen@kekse.biz>
2025-03-05 14:24:38 +0100

    1741181076692135995

--- docs/js/field.md
+++ docs/js/field.md
@@ -1,18 +1,5 @@
 ([Back to the **Index**](./README.md))
 
 # `class Field`
-
-## Constructor
-Can be without any argument or with an arbritary Array type instance.
-Only the first Array type instance will be used in the new Field.
-
-Depends on [Library](./lib.md) function `isArrayType()`. So the regular
-`Array` is possible, but also all known TypedArrays (like the `Uint8Array`).
-
-## `setArray(_value, _check = true)`
-Checks the first argument via [Library](./lib.md) function `isArrayType()`
-(if `_check` is true) and sets **`this.array`** (or throws an Exception).
-
-## `array`
-**Optional** array.
+TODO
 
--- src/js/field.js
+++ src/js/field.js
@@ -1,8 +1,11 @@
 /*
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
  * https://kekse.biz/ https://github.com/kekse1/field/>
- * v0.0.1
+ * v0.0.2
  */
+
+//
+const DEFAULT_BIGINT = true;
 
 //
 import lib from './field.library.js';
@@ -12,31 +15,94 @@ class Field
 {
 	constructor(... _args)
 	{
+		this.bigint = DEFAULT_BIGINT;
 		this.array = null;
+		this.coord = [];
 
-		for(var i = 0; i < _args.length; ++i)
+		for(var i = 0, j = 0; i < _args.length; ++i)
 		{
-			if(lib.isArrayType(_args[i]))
+			if(typeof _args[i] === 'number')
 			{
-				this.setArray(_args.splice(i--, 1)[0], false);
-				break;
+				this.coord[j++] = _args.splice(i--, 1)[0];
+			}
+			else if(lib.isArrayType(_args[i]))
+			{
+				this.array = _args.splice(i--, 1)[0];
+			}
+			else if(typeof _args[i] === 'boolean')
+			{
+				this.bigint = _args.splice(i--, 1)[0];
 			}
 		}
-
-		//
-		//TODO/
-		//
 	}
 
-	setArray(_value, _check = true)
+	setArray(_value)
 	{
-		if(_check && !lib.isArrayType)
+		if(_value === null)
+		{
+			return this.unsetArray();
+		}
+
+		if(!lib.isArrayType)
 		{
 			throw new Error('Value is not an instance of any type of Array.');
 		}
 		
 		return this.array = _value;
 	}
+
+	unsetArray()
+	{
+		const result = !!this.array;
+		this.array = null;
+		return result;
+	}
+
+	resetCoord()
+	{
+		const result = this.coord.length;
+		this.coord = [];
+		return result;
+	}
+
+	setCoord(... _coord)
+	{
+		const result = this.coord.length;
+		this.coord = _coord;
+		return result;
+	}
+
+	addCoord(... _coord)
+	{
+		this.coord.push(... _coord);
+		return this.coord.length;
+	}
+
+	/*getOffset(... _coord)
+	{
+		var result;
+
+		//
+		//todo/
+		//
+
+		//
+		if(this.array)
+		{
+			result %= this.array.length;
+		}
+		
+		return result;
+	}
+
+	getCoord(_offset)
+	{
+		var result;
+
+		//
+		//todo/
+		//
+	}*/
 }
 
 export default Field;
